7cd530c264975929cd6a36576336c33e5b282304
Cesar Alfredo Olvera Benitez <cesar.olvera@b-more.tech>
2024-04-30 12:06:09 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
+
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+ import Dashboard from './components/Dashboard';
+ import Login from './components/Login';
+ import Profile from './components/Profile';
+ import Admin from './components/Admin';
+import Navbar from './components/Navbar';
+import React from 'react';
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HashRouter>
+      
+        <Navbar />
+       
+      <Routes>
+        <Route path='/' element={<Dashboard/>}/>
+        <Route path='/admin' element={<Admin/>}/>
+        <Route path='/profile' element={<Profile/>}/>
+        <Route path='/login' element={<Login/>}/>
+      </Routes>
+    </HashRouter>
+    
+  </div>
   );
 }
 
